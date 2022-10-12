e6c999e80ea2ae3977323e0afcdaafffdcbc9344
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-12 22:10:52 +0800

    add background

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Lab4
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnRefill = new System.Windows.Forms.Button();
             this.txtInfo = new System.Windows.Forms.RichTextBox();
             this.txtOut = new System.Windows.Forms.RichTextBox();
@@ -37,9 +38,9 @@ namespace Lab4
             // 
             // btnRefill
             // 
-            this.btnRefill.Location = new System.Drawing.Point(12, 22);
+            this.btnRefill.Location = new System.Drawing.Point(12, 12);
             this.btnRefill.Name = "btnRefill";
-            this.btnRefill.Size = new System.Drawing.Size(330, 31);
+            this.btnRefill.Size = new System.Drawing.Size(372, 31);
             this.btnRefill.TabIndex = 0;
             this.btnRefill.Text = "Перезаполнить";
             this.btnRefill.UseVisualStyleBackColor = true;
@@ -47,9 +48,9 @@ namespace Lab4
             // 
             // txtInfo
             // 
-            this.txtInfo.Location = new System.Drawing.Point(12, 77);
+            this.txtInfo.Location = new System.Drawing.Point(12, 139);
             this.txtInfo.Name = "txtInfo";
-            this.txtInfo.Size = new System.Drawing.Size(330, 72);
+            this.txtInfo.Size = new System.Drawing.Size(372, 40);
             this.txtInfo.TabIndex = 1;
             this.txtInfo.Text = "";
             this.txtInfo.TextChanged += new System.EventHandler(this.txtInfo_TextChanged);
@@ -58,14 +59,14 @@ namespace Lab4
             // 
             this.txtOut.Location = new System.Drawing.Point(12, 185);
             this.txtOut.Name = "txtOut";
-            this.txtOut.Size = new System.Drawing.Size(201, 96);
+            this.txtOut.Size = new System.Drawing.Size(243, 96);
             this.txtOut.TabIndex = 2;
             this.txtOut.Text = "";
             this.txtOut.TextChanged += new System.EventHandler(this.txtOut_TextChanged);
             // 
             // btnGet
             // 
-            this.btnGet.Location = new System.Drawing.Point(219, 185);
+            this.btnGet.Location = new System.Drawing.Point(261, 185);
             this.btnGet.Name = "btnGet";
             this.btnGet.Size = new System.Drawing.Size(123, 96);
             this.btnGet.TabIndex = 3;
@@ -77,7 +78,9 @@ namespace Lab4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 293);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(396, 293);
             this.Controls.Add(this.btnGet);
             this.Controls.Add(this.txtOut);
             this.Controls.Add(this.txtInfo);
